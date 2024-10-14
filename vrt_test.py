80cd01e196a627d5906dc15fb35f349da8b6658b
Vladimir Shkodin <v.s.shkodin@gmail.com>
2024-10-14 12:14:09 -0400

    Update vrt_test.py

--- vrt_test.py
+++ vrt_test.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import time
 
 # Initialize Playwright and start it.
 playwright = sync_playwright().start()
@@ -8,6 +9,7 @@ browser = playwright.chromium.launch(headless=True)
 # Open a new page (tab) in the browser.
 page = browser.new_page()
 page.goto("https://vshkodin.com/")
+time.sleep(3)
 # Take a screenshot and save it to 'vrt-actual' folder
 page.screenshot(path="vrt-actual/vshkodin_screenshot.png")
 # Close the browser
